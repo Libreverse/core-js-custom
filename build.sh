60226970e8887a4de97a56f594c2f2aaaf60aa14
George Baskerville <148500997+georgebaskervil@users.noreply.github.com>
2024-08-22 02:10:06 +0100

    some renaming

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-bunx babel unifill-source.js -o .temp.js
-bunx google-closure-compiler --js .temp.js --compilation_level SIMPLE --js_output_file unifill.min.js
+bunx babel core-js-source.js -o .temp.js
+bunx google-closure-compiler --js .temp.js --compilation_level SIMPLE --js_output_file core-js-custom.min.js
 rm -rf .temp.js
